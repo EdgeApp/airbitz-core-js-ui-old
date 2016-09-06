798a35ef3bb4757baec51cd060f73066f02ab67b
Paul Puey <paul@airbitz.co>
2016-09-06 14:32:17 -0700

    Only pull in the necessary files from jQuery, bootstrap, and bootstrap-social in prebuild
    Create prebuild.sh which does all the mkdir and copies
    Commit the actual pulled dependencies so prebuild doesn't always have to be done.

---
+++ prebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir -p assets/dist/jquery/dist
+mkdir -p assets/dist/bootstrap/dist/js
+mkdir -p assets/dist/bootstrap/dist/css
+mkdir -p assets/dist/bootstrap-social/assets
+cp -a node_modules/jquery/dist/jquery.min.js assets/dist/jquery/dist/
+cp -a node_modules/bootstrap/dist/css/bootstrap.min.css assets/dist/bootstrap/dist/css/
+cp -a node_modules/bootstrap/dist/js/bootstrap.min.js assets/dist/bootstrap/dist/js/
+cp -a node_modules/bootstrap-social/bootstrap-social.css assets/dist/bootstrap-social/
+cp -a node_modules/bootstrap-social/assets assets/dist/bootstrap-social/
