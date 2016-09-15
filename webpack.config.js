6de4d0c7967849b078d65ad1fe40574f73a77171
Paul Puey <paul@airbitz.co>
2016-09-15 13:41:58 +1200

    Update build with new core-js which points to test-auth

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: { presets: [ 'es2015', 'react' ] }
+      },
+      { 
+        test: /\.json$/, 
+        loader: 'json' 
       }
     ]
   },
