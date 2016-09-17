2035f70487cb976bba06112d353b79b255eeb3ca
Paul Puey <paul@airbitz.co>
2016-09-17 23:10:37 +1200

    Add missing glyph fonts

--- prebuild.sh
+++ prebuild.sh
@@ -2,9 +2,11 @@
 mkdir -p assets/dist/jquery/dist
 mkdir -p assets/dist/bootstrap/dist/js
 mkdir -p assets/dist/bootstrap/dist/css
+mkdir -p assets/dist/bootstrap/dist/fonts
 mkdir -p assets/dist/bootstrap-social/assets
 cp -a node_modules/jquery/dist/jquery.min.js assets/dist/jquery/dist/
 cp -a node_modules/bootstrap/dist/css/bootstrap.min.css assets/dist/bootstrap/dist/css/
 cp -a node_modules/bootstrap/dist/js/bootstrap.min.js assets/dist/bootstrap/dist/js/
+cp -a node_modules/bootstrap/dist/fonts/. assets/dist/bootstrap/dist/fonts/
 cp -a node_modules/bootstrap-social/bootstrap-social.css assets/dist/bootstrap-social/
 cp -a node_modules/bootstrap-social/assets assets/dist/bootstrap-social/
