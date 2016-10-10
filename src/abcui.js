c05d18efdc72067274261dff11e993b2e8d58811
Paul Puey <paul@airbitz.co>
2016-10-10 16:49:13 -0700

    Make abcui.js nodejs compatible

--- src/abcui.js
+++ src/abcui.js
@@ -1,8 +1,27 @@
 var abc = require('airbitz-core-js')
 
+var DomWindow
+var DomDocument
+if (typeof (window) === 'undefined') {
+  DomWindow = {}
+} else {
+  DomWindow = window
+}
+if (typeof (document) === 'undefined') {
+  DomDocument = {
+    createElement: function () {
+      console.log('createElement: Error browser routine used in non-browser environment')
+    },
+    getElementsByTagName: function () {
+      console.log('getElementsByTagName: Error browser routine used in non-browser environment')
+    }
+  }
+} else {
+  DomDocument = document
+}
 function createIFrame (path) {
-  var frame = document.createElement('iframe')
-  var body = document.getElementsByTagName('BODY')[0]
+  var frame = DomDocument.createElement('iframe')
+  var body = DomDocument.getElementsByTagName('BODY')[0]
   body.appendChild(frame, body)
   frame.setAttribute('src', path)
   frame.setAttribute('frameborder', '0')
@@ -24,32 +43,32 @@ function InnerAbcUi (args) {
   if (!apiKey) {
     throw Error('Missing api key')
   }
-  window.abcContext = this.abcContext =
+  DomWindow.abcContext = this.abcContext =
     abc.makeABCContext({'apiKey': args.apiKey, 'accountType': args.accountType})
   if (args['bundlePath']) {
     this.bundlePath = args.bundlePath
   } else {
     this.bundlePath = '/abcui'
   }
-  window.abcuiContext = {
+  DomWindow.abcuiContext = {
     'vendorName': args.vendorName,
     'bundlePath': this.bundlePath
   }
 
   if (typeof args.vendorImageUrl === 'string') {
-    window.abcuiContext.vendorImageUrl = args.vendorImageUrl
+    DomWindow.abcuiContext.vendorImageUrl = args.vendorImageUrl
   } else {
-    window.abcuiContext.vendorImageUrl = ''
+    DomWindow.abcuiContext.vendorImageUrl = ''
   }
 }
 
 InnerAbcUi.prototype.openLoginWindow = function (callback) {
   var frame = createIFrame(this.bundlePath + '/assets/index.html#/login')
   var that = this
-  var abcContext = window.abcContext
-  window.loginCallback = function (error, account, opts) {
+  var abcContext = DomWindow.abcContext
+  DomWindow.loginCallback = function (error, account, opts) {
     if (account) {
-      window.abcAccount = account
+      DomWindow.abcAccount = account
       removeIFrame(frame)
       if (opts && opts.setupRecovery) {
         opts.noRequirePassword = true
@@ -60,7 +79,7 @@ InnerAbcUi.prototype.openLoginWindow = function (callback) {
       callback(error, account)
     }
   }
-  window.exitCallback = function () {
+  DomWindow.exitCallback = function () {
     removeIFrame(frame)
   }
 }
@@ -80,22 +99,22 @@ InnerAbcUi.prototype.openSetupRecoveryWindow = function (account, opts, callback
   } else {
     frame = createIFrame(this.bundlePath + '/assets/index.html#/account/setuprecovery')
   }
-  window.exitCallback = function () {
+  DomWindow.exitCallback = function () {
     removeIFrame(frame)
   }
 }
 
 InnerAbcUi.prototype.openChangePinEdgeLoginWindow = function (account, callback) {
   var frame = createIFrame(this.bundlePath + '/assets/index.html#/account/changepin-edge-login')
-  window.exitCallback = function () {
+  DomWindow.exitCallback = function () {
     removeIFrame(frame)
   }
 }
 
 InnerAbcUi.prototype.openManageWindow = function (account, callback) {
-  window.abcAccount = account
+  DomWindow.abcAccount = account
   var frame = createIFrame(this.bundlePath + '/assets/index.html#/account/')
-  window.exitCallback = function () {
+  DomWindow.exitCallback = function () {
     removeIFrame(frame)
     callback(null)
   }
